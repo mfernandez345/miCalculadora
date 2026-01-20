a079167cd3dc8e4e414d1d04b6573a936501a5a2
mfernandez345 <mfernandez345@users.noreply.github.com>
2026-01-20 12:29:47 +0000

    deploy: 7b0f3dc9166b037885d96d9d74ab720f226bbe21

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"miCalculadora"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"miCalculadora"},{"l":"miCalculadoraTest"}];updateSearchResults();
